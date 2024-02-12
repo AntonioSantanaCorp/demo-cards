b0fb6ac3024956da04c647c33b9b785a6cb1608b
Samu <antoniocorporativo021@gmail.com>
2024-02-12 10:15:05 -0500

    add services

--- src/app/memory-approach/cards-memory-list/cards-memory-list.component.ts
+++ src/app/memory-approach/cards-memory-list/cards-memory-list.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { MemoryService } from '../store/memory.service';
 
 @Component({
   selector: 'app-cards-memory-list',
   standalone: true,
   imports: [],
+  providers:[MemoryService],
   template: `
     <p>
       cards-memory-list works!
