c4d573c7cb49fc1685bc063527a37ddfd7c48fad
Samu <antoniocorporativo021@gmail.com>
2024-02-12 21:21:58 -0500

    changes

--- src/app/shared/card-shop/card-shop.component.ts
+++ src/app/shared/card-shop/card-shop.component.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-card-shop',
   standalone: true,
   imports: [MatCardModule],
-  template: ` <p>card-shop works!</p> `,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  template: `
+    <mat-card>
+      <mat-card-header>
+        <mat-card-title>Shiba Inu</mat-card-title>
+      </mat-card-header>
+      <!-- <img
+        mat-card-image
+        src="https://material.angular.io/assets/img/examples/shiba2.jpg"
+        alt="Photo of a Shiba Inu"
+      /> -->
+      <mat-card-content>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </p>
+      </mat-card-content>
+      <mat-card-actions>
+        <button mat-button>Add favorite</button>
+        <button mat-button>Buy</button>
+      </mat-card-actions>
+    </mat-card>
+  `,
   styles: ``,
 })
 export class CardShopComponent {}
---
+++ src/app/shared/slide-images/slide-images.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-slide-images',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      slide-images works!
+    </p>
+  `,
+  styles: ``
+})
+export class SlideImagesComponent {
+
+}
