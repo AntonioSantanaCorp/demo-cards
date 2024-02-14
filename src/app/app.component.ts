b5f141fbf22c0f64f7b6c459b7aa18a92bf90565
Samu <antoniocorporativo021@gmail.com>
2024-02-14 00:06:10 -0500

    add search input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, OnInit, Output, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { AsyncPipe } from '@angular/common';
+import { AsyncPipe, JsonPipe } from '@angular/common';
 import { StoreService } from './services/products/store-service';
 import { CardShopComponent } from './components/card-shop/card-shop.component';
+import { SearchInputComponent } from './components/search-input/search-input.component';
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -15,11 +16,30 @@ import { CardShopComponent } from './components/card-shop/card-shop.component';
     MatButtonModule,
     MatIconModule,
     CardShopComponent,
+    SearchInputComponent,
     AsyncPipe,
   ],
+  styles: [
+    `
+      .toolbar {
+        gap: 10px;
+      }
+
+      .toolbar__spacer {
+        flex: 1 1 auto;
+      }
+
+      .toolbar__filter-section {
+      }
+    `,
+  ],
   template: `
-    <mat-toolbar color="primary">
+    <mat-toolbar class="toolbar" color="primary">
       <span>Awazon</span>
+      <section class="toolbar__filter-section">
+        <app-search-input (search)="onSearch($event)"></app-search-input>
+      </section>
+      <span class="toolbar__spacer"></span>
       <button mat-icon-button>
         <mat-icon>shopping_cart</mat-icon>
       </button>
@@ -27,17 +47,22 @@ import { CardShopComponent } from './components/card-shop/card-shop.component';
 
     <section class="list-products">
       @for (product of products$ | async; track product) {
-      <app-card-shop [product]="product"></app-card-shop>
+      <app-card-shop [product]="product" />
       }
     </section>
   `,
 })
 export class AppComponent implements OnInit {
-  private readonly _storeService = inject(StoreService);
+  @Output()
+  public readonly _storeService = inject(StoreService);
 
   protected products$ = this._storeService.products$;
 
   ngOnInit(): void {
     this._storeService.connect();
+  }
+
+  onSearch(text: string) {
+    this._storeService.search(text);
   }
 }
--- src/app/components/search-input/search-input.component.ts
+++ src/app/components/search-input/search-input.component.ts
@@ -1,16 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-search-input',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule, MatButtonModule],
+  styles: [
+    `
+      :host {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        background: white;
+        color: black;
+        border-radius: 35px;
+        height: 35px;
+        padding: 0 7px;
+      }
+
+      input {
+        border: none;
+        background-image: none;
+        outline: none;
+        background: transparent;
+        height: 100%;
+        width: 9em;
+        font-size: 15.2px;
+      }
+
+      .clear-btn {
+        cursor: pointer;
+      }
+    `,
+  ],
   template: `
-    <p>
-      search-input works!
-    </p>
+    <mat-icon>search</mat-icon>
+    <input #input (keydown)="search.emit(input.value)" type="text" />
+    <mat-icon
+      (click)="clearSearch(input)"
+      class="clear-btn"
+      fontSet="material-icons-outlined"
+    >
+      cancel
+    </mat-icon>
   `,
-  styles: ``
 })
 export class SearchInputComponent {
+  @Output()
+  public readonly search = new EventEmitter<string>();
 
+  protected clearSearch(input: HTMLInputElement) {
+    this.search.emit('');
+    input.value = '';
+  }
 }
